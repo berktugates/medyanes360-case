62f1780799da7473f7e6de70c958fdf158bc71b4
Berktug Berke Ates <berktugates@gmail.com>
2024-05-25 15:33:07 +0300

    style: grid layout for top box

--- app/components/TopBox.jsx
+++ app/components/TopBox.jsx
@@ -2,7 +2,7 @@ import TopBoxItem from "./TopBoxItem";
 export default function TopBox({ cost, no, date, status }) {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         <TopBoxItem title={"Sipariş Tarihi"} value={date} />
         <TopBoxItem title={"Sipariş Tutarı"} value={`${cost}₺`} />
         <TopBoxItem title={"Sipariş Numarası"} value={no} />
